1fd727d660d4e1ef6ad67941f7d1329ad22606a6
Duncan M <sirduncan887@gmail.com>
2023-04-12 21:03:06 +0300

    this is my first commit

---
+++ Index.php
@@ -0,0 +1,254 @@
+<?php include("header.php"); ?>
+<!doctype html>
+<html lang="en">
+<body>
+    <!-- Hero Section -->
+    <section class="hero mb-5" id="hero">
+        <div class="container text-center container-fluid">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="copy">
+                        <div class="text-label">
+                            Feel at home with super fast internet connection and bring great moments with
+                        </div>
+                        <div class="text-hero-bold">
+                            <span class="text-danger">Home </span> <span class="text-success">Fiber</span>
+                        </div>
+                        <div class="text-hero-regular  mb-3">
+                            supports multiple devices simultaneously<br>catch live football matches and movie streams<br>
+                            Facilitate work meetings, fast web browsing,fast downloads <br>with QTC fiber.
+                        </div>
+                        <div class="cta">
+                            <a href="#" class="btn btn-outline-success shadow d-grip mb-3">Get connected Now</a>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-3">
+                    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="carousel-item active" data-bs-interval="10000">
+                                <img src="imgs/Graphic 1.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="imgs/Graphic 2.png" class="d-block w-100" alt="..." data-bs-interval="5000">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="imgs/graphic 3.png" class="d-block w-100" alt="..." data-bs-interval="2000">
+                            </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                            data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <br>
+    <!-- //Hero section -->
+    <!-- Promo section -->
+    <section class="promo section-padding mt-3" id="promo">
+        <div class="promo-container container-fluid">
+            <div class="row">
+                <div class="promo-content text-center">
+                    <h3 class="text-hero-regular">Featured Offer</h3>
+                    <p class="promo-text">Pay for 2 months<br>get<br><span class="text-danger">Free installation
+                            Now!!!</span></p>
+                    <a href="#" class="btn btn-outline-success shadow d-grip">Get connected Now</a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //Prono section -->
+
+    <!-- About us section -->
+    <section class="about-us mt-5" id="about">
+        <div class="container container-fluid">
+            <div class="row">
+                <div class="col-lg-4 col-md-12 col-12">
+                    <div class="about-image">
+                        <img src="imgs/streaming.png" class="img-fluid mt-5" alt="" srcset="">
+                    </div>
+                </div>
+                <div class="col-lg-8 col-md-12 ps-lg-3 mt-md-5 mt-2">
+                    <div class="about-text text-center">
+                        <h2 class="small-header">We provide the Best Quality <br>Services Ever</h2>
+                        <p class="text-label">At Quantic Networks, we specialize in providing fast, reliable FTTH (Fiber
+                            to the Home) internet services to homes and businesses in our community. Our
+                             fiber-optic network delivers lightning-fast speeds and reliable
+                            connections, so you can stream, browse, and work online without interruptions or slowdowns.
+                            We offer a variety of high-speed internet plans to meet the needs of our customers. Our
+                            plans range from basic to ultra-fast speeds, so you can choose the plan that's right for
+                            you. All of our plans come with unlimited data usage, so you can surf, stream, and work
+                            online without worrying about exceeding your data cap.
+                            In addition to our internet services, we also offer a wide range of networking and
+                            electronic devices for sale. Whether you need a new router, modem, or other networking
+                            equipment, we have you covered. Our expert technicians can help you choose the right
+                            equipment for your needs and budget, and we offer competitive pricing on all of our
+                            products.
+                        </p>
+                        <a href="#" class="btn btn-outline-success shadow d-grip">Explore our Products catalogue</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //About us section -->
+    <!-- Our services section -->
+    <section class="services section-padding mt-5" id="services">
+        <div class="container container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="section-header text-center text-hero-bold">
+                        <h2>Our services</h2>
+                        <p class="text-label">"Explore Our Range of Professional Digital Services at Quantic Networks."
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="card text-white text-center shadow mb-3">
+                        <div class="card-body">
+                            <i class="bi bi-subtract"></i>
+                            <h3 class="small-header">FTTH installation</h3>
+                            <p class="text-hero-regular">We provide professional web
+                                development services to businesses and organizations of all sizes. With expertise in
+                                web development technologies and frameworks, our team can design and develop
+                                custom websites that meet the unique needs of our clients. From e-commerce sites to
+                                corporate websites, we can handle all aspects of the development process, including
+                                website design, coding, testing, and deployment. With a focus on creating user-friendly
+                                and responsive websites, Chapnet Solutions helps businesses establish a strong online
+                                presence and attract more customers.</p>
+                            <a href="#" class="btn btn-outline-success d-grip">Enquire More</a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="card text-white text-center shadow mb-3">
+                        <div class="card-body">
+                            <i class="bi bi-subtract"></i>
+                            <h3 class="small-header">Web development</h3>
+                            <p class="text-hero-regular">We provide professional web
+                                development services to businesses and organizations of all sizes. With expertise in
+                                web development technologies and frameworks, our team can design and develop
+                                custom websites that meet the unique needs of our clients. From e-commerce sites to
+                                corporate websites, we can handle all aspects of the development process, including
+                                website design, coding, testing, and deployment. With a focus on creating user-friendly
+                                and responsive websites, Quantic networks helps businesses establish a strong online
+                                presence and attract more customers.</p>
+                            <a href="#" class="btn btn-outline-success d-grip">Enquire More</a>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-12 col-md-12 col-lg-4">
+                    <div class="card text-white text-center shadow mb-3">
+                        <div class="card-body">
+                            <i class="bi bi-subtract"></i>
+                            <h3 class="small-header">Digital Marketing</h3>
+                            <p class="text-hero-regular">Our team of Elite marketing experts offer comprehensive digital
+                                marketing
+                                services that help businesses reach their target audience, increase brand awareness,<br>
+                                and
+                                drive more conversions. We use a variety of
+                                techniques, including search engine marketing, social media marketing, email marketing,
+                                and content marketing, to attract and engage customers. With a data-driven approach,<br>
+                                Chapnet Solutions continuously measures and optimizes our digital marketing campaigns
+                                to deliver maximum ROI for our clients. <br><br></p>
+                            <a href="#" class="btn btn-outline-success d-grip">Enquire More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //Our services section -->
+    
+    <!-- Contact Us section -->
+    <section class="section-padding mt-5" id="contact-us">
+        <div class="section-header">
+            <h3 class="text-center small-header">Get in Touch</h3>
+        </div>
+        <div class="container mt-5 container-fluid shadow b-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="mb-3">
+                        <label for="email" class="form-label text-hero-regular text-success">Email address</label>
+                        <input type="email" class="form-control" id="email" placeholder="name@example.com">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlTextarea1"
+                            class="form-label text-hero-regular text-success">Message</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <button class="btn btn-outline-success" type="button">Send</button>
+                    </div>
+                </div>
+                <div class="col-md-6 mt-5 mb-3">
+                    <div class="contact-us text-center" id="contact-us">
+                        <h2 class="text-danger smaller-header">Contact Us</h2>
+                        <div class="mt-5 text-center">
+                            <div class="d-flex">
+                                <i class="bi bi-geo-alt-fill fs-3 text-danger "></i>
+                                <p class="text-paragraph mx-3">Address: 02002-19752 Nairobi, Kenya</p>
+                            </div>
+                            <div class="d-flex">
+                                <i class="bi bi-envelope fs-3 text-danger"></i>
+                                <p class="text-paragraph mx-3">Email: quanticfiber@gmail.com</p>
+                            </div>
+                            <div class="d-flex">
+                                <i class="bi bi-globe fs-3 text-danger"></i>
+                                <p class="text-paragraph mx-3">Website: www.quanticnetworks.co.ke</p>
+                            </div>
+                            <div class="d-flex">
+                                <i class="bi bi-telephone fs-3 text-danger"></i>
+                                <p class="text-paragraph mx-3">Telephone: 0790737555/0114063049</p>
+                            </div>
+                            <div class="d-flex justify-content-evenly fs-3 ">
+                                <a href="#"><i class="bi bi-facebook text-danger"></i></a>
+                                <a href="#"><i class="bi bi-linkedin text-danger"></i></a>
+                                <a href="#"> <i class="bi bi-twitter text-danger"></i></a>
+                                <a href="#"><i class="bi bi-instagram text-danger"></i></a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- //Contact Us section -->
+    <div class="footer mt-5  container-fluid">
+        <div class="container ">
+            <div class="row">
+                <div class="text-footer text-center">
+                    Copyright &copy; Quantic<span class="text-danger"> Networks</span> 2023
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- Popper.js, then Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
